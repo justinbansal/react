b2e0f00c886ec53013f5abf8e355590401711894
Justin <justinbansal@gmail.com>
2024-06-13 18:55:12 -0400

    Sets up hidden class and checks for match

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,44 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 import Card from './Card';
 
 function App() {
+  const [choices, setChoices] = useState([]);
+  const [isSelected, setIsSelected] = useState(false);
+  const [isMatch, setIsMatch] = useState(false);
+
+  useEffect(() => {
+    if (choices.length > 0 && choices[0] && choices[1]) {
+      if (choices[0].content === choices[1].content) {
+        alert('We have a match!');
+      }
+    }
+  }, [choices])
+
+  function handleClick(e) {
+    if (choices.length > 1) return;
+
+    setChoices([...choices, {id: e.target.id, content: e.target.dataset.value}]);
+  }
+
   return (
     <>
       <div>
         <h1>Memory Game</h1>
         <div className="board">
-          <Card content="A" />
-          <Card content="B" />
-          <Card content="C" />
-          <Card content="B" />
-          <Card content="D" />
-          <Card content="E" />
-          <Card content="C" />
-          <Card content="F" />
-          <Card content="F" />
-          <Card content="E" />
-          <Card content="A" />
-          <Card content="D" />
+          <Card content="A" handleClick={(e) => handleClick(e)} choices={choices} id="1" />
+          <Card content="B" handleClick={(e) => handleClick(e)} choices={choices} id="2" />
+          <Card content="C" handleClick={(e) => handleClick(e)} choices={choices} id="3" />
+          <Card content="B" handleClick={(e) => handleClick(e)} choices={choices} id="4" />
+          <Card content="D" handleClick={(e) => handleClick(e)} choices={choices} id="5" />
+          <Card content="E" handleClick={(e) => handleClick(e)} choices={choices} id="6" />
+          <Card content="C" handleClick={(e) => handleClick(e)} choices={choices} id="7" />
+          <Card content="F" handleClick={(e) => handleClick(e)} choices={choices} id="8" />
+          <Card content="F" handleClick={(e) => handleClick(e)} choices={choices} id="9" />
+          <Card content="E" handleClick={(e) => handleClick(e)} choices={choices} id="10" />
+          <Card content="A" handleClick={(e) => handleClick(e)} choices={choices} id="11" />
+          <Card content="D" handleClick={(e) => handleClick(e)} choices={choices} id="12" />
         </div>
       </div>
     </>
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,6 +1,15 @@
 function Card(props) {
+  let visibility = props.choices.length > 0 && props.choices.find(choice => choice.id === props.id) ? 'visible' : 'hidden';
+
   return (
-    <div className={`card ${props.content}`}>{props.content}</div>
+    <div
+      className={`card ${props.content} ${visibility}`}
+      onClick={(e) => props.handleClick(e)}
+      data-value={props.content}
+      id={props.id}
+    >
+      {props.content}
+    </div>
   )
 }
 
