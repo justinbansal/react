2958801f0c41cee0f7a03757dfca5a1ab4f75419
Justin <justinbansal@gmail.com>
2024-06-15 15:53:28 -0400

    Got match and check for two selected cards logic to work

--- src/App.jsx
+++ src/App.jsx
@@ -56,10 +56,51 @@ function App() {
     }
   ])
 
+  const isMatch = function checkCardsForMatch() {
+    const selectedCards = cards.filter(card => card.selected);
+    if (selectedCards[0].content === selectedCards[1].content) {
+      selectedCards[0].matched = true;
+      selectedCards[1].matched = true;
+      return true;
+    }
+  }
+
+  const twoCardsSelected = function countSelected() {
+    let selectedCards = cards.filter(card => card.selected);
+    if (selectedCards.length === 2) {
+      return true;
+    }
+  }
+
   function handleClick(i) {
+    if (twoCardsSelected()) return;
     const updatedCards = cards.slice();
     updatedCards[i].selected = !cards[i].selected;
     setCards(updatedCards);
+    if (twoCardsSelected()) {
+      if (isMatch()) {
+        let updatedCards = cards.map(card => {
+          if (card.selected === true) {
+            card.selected = false;
+            return card;
+          }
+          return card;
+        });
+        setCards(updatedCards);
+      } else {
+        setTimeout(() => {
+          let updatedCards = cards.map(card => {
+            if (card.selected === true) {
+              card.selected = false;
+              return card;
+            }
+            return card;
+          });
+          setCards(updatedCards);
+        }, 1500);
+        return;
+      }
+    }
   }
 
   return (
@@ -67,18 +108,18 @@ function App() {
       <div>
         <h1>Memory Game</h1>
         <div className="board">
-          <Card content={cards[0].content} handleClick={() => handleClick(0)} isSelected={cards[0].selected} />
-          <Card content={cards[1].content} handleClick={() => handleClick(1)} isSelected={cards[1].selected} />
-          <Card content={cards[2].content} handleClick={() => handleClick(2)} isSelected={cards[2].selected} />
-          <Card content={cards[3].content} handleClick={() => handleClick(3)} isSelected={cards[3].selected} />
-          <Card content={cards[4].content} handleClick={() => handleClick(4)} isSelected={cards[4].selected} />
-          <Card content={cards[5].content} handleClick={() => handleClick(5)} isSelected={cards[5].selected} />
-          <Card content={cards[6].content} handleClick={() => handleClick(6)} isSelected={cards[6].selected} />
-          <Card content={cards[7].content} handleClick={() => handleClick(7)} isSelected={cards[7].selected} />
-          <Card content={cards[8].content} handleClick={() => handleClick(8)} isSelected={cards[8].selected} />
-          <Card content={cards[9].content} handleClick={() => handleClick(9)} isSelected={cards[9].selected} />
-          <Card content={cards[10].content} handleClick={() => handleClick(10)} isSelected={cards[10].selected} />
-          <Card content={cards[11].content} handleClick={() => handleClick(11)} isSelected={cards[11].selected} />
+          <Card content={cards[0].content} handleClick={() => handleClick(0)} isSelected={cards[0].selected} isMatched={cards[0].matched} />
+          <Card content={cards[1].content} handleClick={() => handleClick(1)} isSelected={cards[1].selected} isMatched={cards[1].matched} />
+          <Card content={cards[2].content} handleClick={() => handleClick(2)} isSelected={cards[2].selected} isMatched={cards[2].matched} />
+          <Card content={cards[3].content} handleClick={() => handleClick(3)} isSelected={cards[3].selected} isMatched={cards[3].matched} />
+          <Card content={cards[4].content} handleClick={() => handleClick(4)} isSelected={cards[4].selected} isMatched={cards[4].matched} />
+          <Card content={cards[5].content} handleClick={() => handleClick(5)} isSelected={cards[5].selected} isMatched={cards[5].matched} />
+          <Card content={cards[6].content} handleClick={() => handleClick(6)} isSelected={cards[6].selected} isMatched={cards[6].matched} />
+          <Card content={cards[7].content} handleClick={() => handleClick(7)} isSelected={cards[7].selected} isMatched={cards[7].matched} />
+          <Card content={cards[8].content} handleClick={() => handleClick(8)} isSelected={cards[8].selected} isMatched={cards[8].matched} />
+          <Card content={cards[9].content} handleClick={() => handleClick(9)} isSelected={cards[9].selected} isMatched={cards[9].matched} />
+          <Card content={cards[10].content} handleClick={() => handleClick(10)} isSelected={cards[10].selected} isMatched={cards[10].matched} />
+          <Card content={cards[11].content} handleClick={() => handleClick(11)} isSelected={cards[11].selected} isMatched={cards[11].matched} />
         </div>
       </div>
     </>
