a2afc4fb4a28f6b21d7630230e1ecb9da3ba0c34
Justin <justinbansal@gmail.com>
2024-06-23 18:18:09 -0400

    Connect dropdown to state

--- src/ContentSelector.tsx
+++ src/ContentSelector.tsx
@@ -1,12 +1,13 @@
 interface ContentSelectorProps {
   handleSelectChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+  contentType: string
 }
 
 function ContentSelector(props: ContentSelectorProps) {
   return (
     <div className="select-wrapper">
       <label htmlFor="content">Choose content type:</label>
-      <select name="content" id="content" onChange={e => props.handleSelectChange(e)}>
+      <select name="content" id="content" onChange={e => props.handleSelectChange(e)} value={props.contentType}>
         <option value="default">Default</option>
         <option value="animals">Animals</option>
         <option value="cars">Luxury Car Logos</option>
